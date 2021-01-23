b8343b2dc6ce7c8cb1dc41c9392cbed74c798ddb
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-01-23 13:26:00 -0300

    feat: Add some events

--- src/client/index.js
+++ src/client/index.js
@@ -1,21 +1,22 @@
 import './styles/app.css';
 import data from './js/dino.json'; 
 import AnimalSpec from './js/AnimalSpec';
-import Inventory from './js/Inventory';
+import inventory from './js/inventory';
 
 (function(){
   
-  const inventory = new Inventory();
+  const formElement = document.querySelector('form');
+  const buttonElement = document.querySelector('#btn');
+  
   for(const {species, weight, height, diet, where, when, fact} of data.Dinos) {
     const spec = new AnimalSpec(weight, height, diet);
     inventory.addDino(species, where, when, fact, spec);
   }
-
-  console.log(
-    JSON.stringify(
-      inventory.valueOf()
-    )
-  );
+ 
+  buttonElement.addEventListener("click", function submitForm(e) {
+    e.preventDefault();
+    formElement.classList.toggle('hide');
+  });
   
 
 })();
