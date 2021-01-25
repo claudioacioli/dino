0485fd2a9b3640e2b1e4aec3b71712bbabd9da17
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-01-25 20:52:46 -0300

    chore: Set development mode for webpack

---
+++ webpack.dev.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/client/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Development',
+      template: './src/client/views/index.html'
+    }),
+    new CleanWebpackPlugin()
+  ],
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpgeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+};
