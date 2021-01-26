b7e194329913505096af20a496c33f6176590884
Claudio Acioli <claudio.acioli.silva@gmail.com>
2021-01-25 21:35:32 -0300

    feat: Add some events

--- src/client/index.js
+++ src/client/index.js
@@ -2,21 +2,49 @@ import './styles/app.css';
 import data from './js/dino.json'; 
 import AnimalSpec from './js/AnimalSpec';
 import inventory from './js/inventory';
+import gridComponent from './js/grid';
 
 (function(){
   
-  const formElement = document.querySelector('form');
-  const buttonElement = document.querySelector('#btn');
-  
-  for(const {species, weight, height, diet, where, when, fact} of data.Dinos) {
-    const spec = new AnimalSpec(weight, height, diet);
-    inventory.addDino(species, where, when, fact, spec);
-  }
- 
-  buttonElement.addEventListener("click", function submitForm(e) {
+  const
+    // Elements
+    formElement = document.querySelector('form'),
+    buttonElement = document.querySelector('#btn'),
+    
+    // Helpers
+    initObjects = () => {
+      for(const {species, weight, height, diet, where, when, fact} of data.Dinos) {
+        const spec = new AnimalSpec(weight, height, diet);
+        inventory.addDino(species, where, when, fact, spec);
+      }
+    },
+
+    // Render Functions
+    renderGrid = e => {
+      
+      const items = inventory.getAllDinos().map(({species, fact}) => ({
+        title: species,
+        src: species.toLowerCase(),
+        fact
+      }));
+      
+      gridComponent(items);
+
+    }
+  ;
+
+  initObjects();
+
+  formElement.addEventListener("submit", function submitForm(e) {
     e.preventDefault();
-    formElement.classList.toggle('hide');
+    formElement.classList.toggle("hide");
+    renderGrid();
+    formElement.reset();
+  });
+
+  buttonElement.addEventListener("click", function clickButton(e) {
+    e.preventDefault();
+    formElement.requestSubmit();
   });
   
-
 })();
